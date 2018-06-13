f6d552c792e005ee7331ec07b64332cbdb0103aa
Johannes Luderschmidt <lagerkoller@vildan.de>
2018-06-13 14:03:47 +0200

    Initial commit

--- ManualDeviceOrientation/ViewController.swift
+++ ManualDeviceOrientation/ViewController.swift
@@ -7,18 +7,43 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var orientationLabel: UILabel!
+    let motionManager = CMMotionManager()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        motionManager.startAccelerometerUpdates()
+        
+        if motionManager.isAccelerometerAvailable {
+            motionManager.accelerometerUpdateInterval = 0.1
+            motionManager.startAccelerometerUpdates(to: .main) {
+                (data: CMAccelerometerData?, error: Error?) in
+                if let acceleration = data?.acceleration {
+                    let rotation = atan2(acceleration.x, acceleration.y) - .pi
+                    if rotation <= -1.75 * .pi  || rotation > -0.25 * .pi {
+                        self.orientationLabel.text = "Portrait up"
+                    } else if rotation <= -0.25 * .pi  && rotation > -0.75 * .pi {
+                        self.orientationLabel.text = "Landscape right"
+                    } else if rotation <= -0.75 * .pi  && rotation > -1.25 * .pi {
+                        self.orientationLabel.text = "Portrait down"
+                    } else if rotation <= -1.25 * .pi  && rotation > -1.75 * .pi {
+                        self.orientationLabel.text = "Landscape left"
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        motionManager.stopAccelerometerUpdates()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
